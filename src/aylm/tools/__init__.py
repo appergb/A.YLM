d536e9e854e9823bb42381aedb19aec0c0a71c02
TRIP(appergb) <appergb@github.com>
2026-01-13 20:30:40 +0800

    style: 修复CI代码格式化检查失败问题
    - 使用isort修复src/aylm/tools/__init__.py导入格式
    - 将多行导入语句优化为单行格式
    - 确保代码符合项目预设的isort规范
    - 解决CI中'1 file would be reformatted'错误

--- src/aylm/tools/__init__.py
+++ src/aylm/tools/__init__.py
@@ -1,12 +1,8 @@
 """A.YLM工具模块."""
 
 # 导入所有工具模块，使其可以通过 aylm.tools.module_name 访问
-from . import (
-    coordinate_utils,
-    pointcloud_voxelizer,
-    preload_sharp_model,
-    undistort_iphone,
-)
+from . import (coordinate_utils, pointcloud_voxelizer, preload_sharp_model,
+               undistort_iphone)
 
 __all__ = [
     "coordinate_utils",
